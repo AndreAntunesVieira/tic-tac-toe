de56d49ff054ac6d962905644801b0d0ae823672
Andre Antunes Vieira <andreantunesv@gmail.com>
2019-12-09 01:02:05 -0300

    Mark fields and threat users

--- frontend.js
+++ frontend.js
@@ -1,106 +1,103 @@
-const log = document.querySelector("#log");
-const input = document.querySelector("#input");
-const status = document.querySelector("#status");
+const log = document.querySelector('#log')
+const input = document.querySelector('#input')
+const status = document.querySelector('#status')
+const field = document.querySelector('#field')
 
-let myColor = false;
-let myName = false;
+const myState = {
+  name: null,
+  type: 'none',
+}
 
-window.WebSocket = window.WebSocket || window.MozWebSocket;
+window.WebSocket = window.WebSocket || window.MozWebSocket
 
-if (!window.WebSocket)
-  log.innerHTML = "Sorry, but your browser doesn't support WebSocket.";
+if (!window.WebSocket) log.innerHTML = "Sorry, but your browser doesn't support WebSocket."
 
-const connection = new WebSocket("ws://127.0.0.1:1337");
-connection.onopen = function() {
-  input.removeAttribute("disabled");
-  status.innerText = "Choose name:";
-};
+const connection = new WebSocket('ws://127.0.0.1:1337')
+
+connection.onopen = function(e) {
+  console.log(e)
+  input.removeAttribute('disabled')
+  status.innerText = 'Choose name:'
+}
+
 connection.onerror = function(error) {
   console.log(error)
-  log.innerHTML = "Sorry, but there's some problem with your connection or the server is down."
-};
+  log.innerHTML = 'Erro ao conectar com o servidor'
+}
 
 connection.onmessage = function(message) {
-  console.log(message)
-  // try {
-  //   var json = JSON.parse(message.data);
-  // } catch (e) {
-  //   console.log("Invalid JSON: ", message.data);
-  //   return;
-  // }
-  // // NOTE: if you're not sure about the JSON structure
-  // // check the server source code above
-  // // first response from the server with user's color
-  // if (json.type === "color") {
-  //   myColor = json.data;
-  //   status.text(myName + ": ").css("color", myColor);
-  //   input.removeAttr("disabled").focus();
-  //   // from now user can start sending messages
-  // } else if (json.type === "history") {
-  //   // entire message history
-  //   // insert every single message to the chat window
-  //   for (var i = 0; i < json.data.length; i++) {
-  //     addMessage(
-  //       json.data[i].author,
-  //       json.data[i].text,
-  //       json.data[i].color,
-  //       new Date(json.data[i].time)
-  //     );
-  //   }
-  // } else if (json.type === "message") {
-  //   // it's a single message
-  //   // let the user write another message
-  //   input.removeAttr("disabled");
-  //   addMessage(
-  //     json.data.author,
-  //     json.data.text,
-  //     json.data.color,
-  //     new Date(json.data.time)
-  //   );
-  // } else {
-  //   console.log("Hmm..., I've never seen JSON like this:", json);
-  // }
-// };
+  const data = JSON.parse(message.data)
+  console.log(data)
+  if (data.type === 'bootstrap') return bootstrapLogs(data.history)
+  if (data.type === 'mark') return receiveMark(data)
+  if (data.type === 'historyPush') return (log.innerHTML = log.innerHTML + `<div>${data.message}</div>`)
+  if (data.type === 'clearBoard') return clearBoard()
+  if (data.type === 'confirm') {
+    Object.assign(myState, data.player)
+    status.innerHTML = `Seu nome é <b>${myState.name}</b> e você é o jogador <b>${typeName()}</b>`
+  }
+}
 
-// input.keydown(function(e) {
-//   if (e.keyCode === 13) {
-//     var msg = $(this).val();
-//     if (!msg) {
-//       return;
-//     }
-//     // send the message as an ordinary text
-//     connection.send(msg);
-//     $(this).val("");
-//     // disable the input field to make the user wait until server
-//     // sends back response
-//     input.attr("disabled", "disabled");
-//     // we know that the first message sent from a user their name
-//     if (myName === false) {
-//       myName = msg;
-//     }
-//   }
-// });
+input.addEventListener('keydown', function(e) {
+  if (e.key === 'Enter') {
+    const name = input.value.trim()
+    if (!name || myState.name) return
+
+    sendMessage({ type: 'setUser', user: { name } })
+    input.value = ''
+
+    myState.type = 'confirmando...'
+    myState.name = name
+    status.innerHTML = `Seu nome é <b>${name}</b> e você é o jogador <b>${typeName()}</b>`
+    status.style.textAlign = 'center'
+    status.style.display = 'block'
+    input.setAttribute('disabled', 'disabled')
+    input.style.display = 'none'
+  }
+})
+
+field.querySelectorAll('div').forEach(section => {
+  section.addEventListener('click', e => {
+    const element = e.target
+    const index = Array.from(element.parentNode.children).indexOf(element)
+    sendMessage({ type: 'mark', index })
+  })
+})
+
+function clearBoard(){
+  field.querySelectorAll('div').forEach(section => {
+    section.classList.remove('o', 'x')
+  })
+}
+
+function receiveMark(data) {
+  if (!data.user) return null
+  console.log(data)
+  const element = field.querySelectorAll('div')[data.index]
+  element.classList.add(data.user)
+}
+
+function sendMessage(data) {
+  connection.send(JSON.stringify(data))
+}
+
+function typeName() {
+  if (myState.type === 'o') return 'bolinha'
+  return 'xis'
+}
 
 setInterval(function() {
   if (connection.readyState !== 1) {
-    status.text("Error");
-    input.setAttribute("disabled", "disabled")
-    input.value = "Unable to communicate with the WebSocket server.";
+    status.innerHTML = 'Error'
+    input.setAttribute('disabled', 'disabled')
+    input.value = 'Unable to communicate with the WebSocket server.'
   }
 }, 3000)
 
-// function addMessage(author, message, color, dt) {
-//   log.prepend(
-//     '<p><span style="color:' +
-//       color +
-//       '">' +
-//       author +
-//       "</span> @ " +
-//       (dt.getHours() < 10 ? "0" + dt.getHours() : dt.getHours()) +
-//       ":" +
-//       (dt.getMinutes() < 10 ? "0" + dt.getMinutes() : dt.getMinutes()) +
-//       ": " +
-//       message +
-//       "</p>"
-//   );
+function bootstrapLogs(history) {
+  let fullMessage = ''
+  history.forEach(message => {
+    fullMessage = fullMessage + `<div>${message}</div>`
+  })
+  log.innerHTML = fullMessage
 }
--- server.js
+++ server.js
@@ -1,91 +1,107 @@
-"use strict";
-process.title = 'node-chat';
+'use strict'
+process.title = 'node-chat'
 
-var webSocketsServerPort = 1337;
+const WS_PORT = 1337
 
-var webSocketServer = require('websocket').server;
-var http = require('http');
+const webSocketServer = require('websocket').server
+const http = require('http')
 
-var history = [ ];
-
-var clients = [ ];
-
-function htmlEntities(str) {
-  return String(str)
-    .replace(/&/g, '&amp;').replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+const game = {
+  x: null,
+  o: null,
+  board: {},
+  turn: null,
 }
+const states = {}
+const history = []
+const server = http.createServer(function(request, response) {})
 
-var colors = [ 'red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange' ];
+server.listen(WS_PORT, () => console.log(new Date() + ' Server is listening on port ' + WS_PORT))
 
-colors.sort(function(a,b) { return Math.random() > 0.5; } );
-
-var server = http.createServer(function(request, response) {
-
-});
-server.listen(webSocketsServerPort, function() {
-  console.log((new Date()) + " Server is listening on port "
-    + webSocketsServerPort);
-});
-
-var wsServer = new webSocketServer({
-  httpServer: server
-});
+const wsServer = new webSocketServer({ httpServer: server })
+const randString = () => Math.round(Math.random() * 10000).toString()
 
 wsServer.on('request', function(request) {
-  console.log((new Date()) + ' Connection from origin '
-    + request.origin + '.');
+  console.log(`${new Date()} Connection from origin ${request.origin}.`)
 
-  var connection = request.accept(null, request.origin);
+  const id = generateId()
+  const connection = request.accept(null, request.origin)
+  const send = setSender(connection)
 
-  var index = clients.push(connection) - 1;
-  var userName = false;
-  var userColor = false;
-  console.log((new Date()) + ' Connection accepted.');
+  states[id] = { connection }
 
-  if (history.length > 0) {
-    connection.sendUTF(
-      JSON.stringify({ type: 'history', data: history} ));
-  }
+  console.log(`${new Date()} Connection accepted.`)
+
+  send({ type: 'bootstrap', history })
 
   connection.on('message', function(message) {
-    if (message.type === 'utf8') { // accept only text
-      if (userName === false) {
-        userName = htmlEntities(message.utf8Data);
-        userColor = colors.shift();
-        connection.sendUTF(
-          JSON.stringify({ type:'color', data: userColor }));
-        console.log((new Date()) + ' User is known as: ' + userName
-          + ' with ' + userColor + ' color.');
-      } else {
-        console.log((new Date()) + ' Received Message from '
-          + userName + ': ' + message.utf8Data);
+    if (message.type !== 'utf8') return null
+    const data = JSON.parse(message.utf8Data)
+    if (data.type === 'setUser') return setUser(id, data.user)
+    if (data.type === 'mark') return mark(id, data.index)
+  })
 
-        var obj = {
-          time: (new Date()).getTime(),
-          text: htmlEntities(message.utf8Data),
-          author: userName,
-          color: userColor
-        };
-        history.push(obj);
-        history = history.slice(-100);
+  connection.on('close', function() {
+    if (!states[id].name) return null
+    disconnectUser(id)
+  })
+})
 
-        var json = JSON.stringify({ type:'message', data: obj });
-        for (var i=0; i < clients.length; i++) {
-          clients[i].sendUTF(json);
-        }
-      }
-    }
-  });
+function setUser(id, user) {
+  const state = states[id]
+  const send = setSender(state.connection)
+  Object.assign(state, user)
+  if (!game.x) {
+    game.x = { id }
+    state.type = 'x'
+  } else if (!game.o) {
+    game.o = { id }
+    state.type = 'o'
+  }
+  if (game.x && game.o && !game.turn) game.turn = 'x'
+  const historyMessage = `User ${state.name} connected as ${state.type}`
+  send({ type: 'confirm', player: { type: state.type } })
+  broadcastData({ type: 'historyPush', message: historyMessage })
+}
 
-  connection.on('close', function(connection) {
-    if (userName !== false && userColor !== false) {
-      console.log((new Date()) + " Peer "
-        + connection.remoteAddress + " disconnected.");
+function disconnectUser(id) {
+  const state = states[id]
+  console.log(`${new Date()} Peer ${state.connection.remoteAddress} disconnected.`)
+  if (!state.type) return broadcastData({ type: 'historyPush', message: `User ${state.name || state.id} disconnected` })
+  broadcastHistory(`User ${state.name} (${state.type}) disconnected`)
+  broadcastData({ type: 'clearBoard' })
+  game[state.type] = null
+  delete states[id]
+}
 
-      clients.splice(index, 1);
+function setSender(connection) {
+  return data => connection.send(JSON.stringify(data))
+}
 
-      colors.push(userColor);
-    }
-  });
-});
+function mark(id, index) {
+  const state = states[id]
+  if (game.board[index] || state.type !== game.turn) {
+    const blockedMessage = `Jogador ${state.name} (${state.type}) tentou marcar na posição ${index} e foi bloqueado.`
+    return broadcastHistory(blockedMessage)
+  }
+  broadcastData({ type: 'mark', user: state.type, index })
+  broadcastHistory(`Jogador ${state.name} (${state.type}) marcou posição ${index}`)
+  game.board[index] = state.type
+  game.turn = state.type === 'o' ? 'x' : 'o'
+}
+
+function broadcastHistory(message) {
+  history.push(message)
+  return broadcastData({ type: 'historyPush', message })
+}
+
+function broadcastData(data) {
+  for (const id in states) {
+    const client = states[id]
+    client.connection.send(JSON.stringify(data))
+  }
+}
+
+function generateId() {
+  return new Date().getTime().toString() + randString()
+}
